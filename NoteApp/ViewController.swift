8ade61b2875d4c2f4b33d367254bacf5de097d8e
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-11-05 11:57:17 +0400

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Mariam Joglidze on 05.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
